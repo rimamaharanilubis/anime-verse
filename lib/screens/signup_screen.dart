10f96f1800be8df6f4b5d3363c80c6184d105bd1
RimaLubis <maharanilubis250305@gmail.com>
2025-12-06 14:10:26 +0700

    Add screenshots

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,4 +1,4 @@
-import 'package:anime_verse/widgets/app_scaffold.dart';
+ import 'package:anime_verse/widgets/app_scaffold.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
